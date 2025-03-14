33d9a83a3a0bdc2d3ba5800e3fc0a277cfe0f517
Ahmed-Elimam <aa.emam97@gmail.com>
2025-03-14 22:56:19 +0200

    after adding vite finally

--- resources/views/Posts/show.blade.php
+++ resources/views/Posts/show.blade.php
@@ -5,9 +5,7 @@
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
    <meta http-equiv="X-UA-Compatible" content="ie=edge">
    <title>Show Post</title>
-   <script src="https://unpkg.com/@tailwindcss/browser@4"></script>
-   <!-- @vite(['resources/css/app.css', 'resources/js/app.js']) -->
-
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body class="bg-gray-50">
    <!-- Navigation -->
--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -5,8 +5,8 @@
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
    <meta http-equiv="X-UA-Compatible" content="ie=edge">
    <title>{{ $title ?? 'ITI Blog Post' }}</title>
-   <script src="https://unpkg.com/@tailwindcss/browser@4"></script>
-</head>
+   @vite(['resources/css/app.css', 'resources/js/app.js'])
+   </head>
 <body class="bg-gray-50">
    <!-- Navigation -->
    <nav class="bg-white shadow">
